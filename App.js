b3e24051cf74e35a922ee6121b07c97141831a85
Sahibjeet Singh <sahibsingh318.ss@gmail.com>
2023-06-15 21:38:45 -0400

    Miscellaneous changes

--- App.js
+++ App.js
@@ -1,36 +1,13 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-//import firebase from './firebase';
 import HomePage from './components/HomePage';
 import Settings from './components/Settings';
 import CoursesPage from './components/CoursesPage';
 import CourseDetailsPage from './components/CourseDetailsPage';
-//import Carousel from 'react-native-snap-carousel';
-import { View } from 'react-native';
-import PageHeader from './components/HomePageHeader';
-//import { ViewPropTypes } from 'react-native';
+import { StyleSheet } from 'react-native';
 import StartPage from './components/StartPage';
 import SignupPage from './components/SignupPage';
-
-/*// Write data to the database
-firebase.database().ref('users/1').set({
-  name: 'John Doe',
-  age: 25,
-});
-
-// Read data from the database
-firebase
-  .database()
-  .ref('users/1')
-  .once('value')
-  .then((snapshot) => {
-    const user = snapshot.val();
-    console.log(user);
-  })
-  .catch((error) => {
-    console.log('Error fetching data:', error);
-  });*/
 
 
 const Stack = createStackNavigator();
@@ -43,7 +20,6 @@ const App = () => {
           component={StartPage}
           options={{ title: 'GOIELTS Learning App' }}
         />
-        <Stack.Screen
         <Stack.Screen style={styles.Heading}
           name="Home"
           component={HomePage}
@@ -74,7 +50,7 @@ const App = () => {
   );
 };
 
-const styles = styles.create({
+const styles = StyleSheet.create({
   Heading: {
     flex: 1,
     alignItems: 'center',
